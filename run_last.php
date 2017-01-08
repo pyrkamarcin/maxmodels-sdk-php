3bdc2af1afdc5d328eb57da09da894cb3c9d3510
JetAlone <pyrka.marcin@gmail.com>
2017-01-08 23:14:37 +0100

    Update

--- run_last.php
+++ run_last.php
@@ -10,11 +10,5 @@ $maxer->login($username, $password);
 $photos = $maxer->getLastPhotos(10);
 foreach ($photos as $photo) {
     $vouteResults = $maxer->vouter($photo);
-    sleep(5);
-}
-
-$photos = $maxer->getObservedtPhotos(20);
-foreach ($photos as $photo) {
-    $vouteResults = $maxer->vouter($photo);
-    sleep(5);
+    sleep(10);
 }
---
+++ run_observed.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/config.php';
+
+$maxer = new \Maxer\Maxer();
+
+$maxer->login($username, $password);
+
+$photos = $maxer->getObservedtPhotos(20);
+foreach ($photos as $photo) {
+    $vouteResults = $maxer->vouter($photo);
+    sleep(30);
+}
