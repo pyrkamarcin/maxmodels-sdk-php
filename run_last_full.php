bf08b0b776a255a4425969ac4d9d991b16161849
JetAlone <pyrka.marcin@gmail.com>
2017-01-12 23:06:01 +0100

    Update

--- run_last_full.php
+++ run_last_full.php
@@ -7,14 +7,17 @@ $maxer = new \Maxer\Maxer();
 $maxer->login($username, $password);
 
 $users = new \Maxer\API\Request\UserRequest();
-$users = \Maxer\API\Response\UserResponse::toObjects(\Maxer\API\Response\UserResponse::parse($users->execute(), 5));
+$users = \Maxer\API\Response\UserResponse::toObjects(\Maxer\API\Response\UserResponse::parse($users->execute(), 15));
 
 foreach ($users as $user) {
 
-    $photos = $maxer->getUserPhotos($users[0], 10);
+    $photos = $maxer->getUserPhotos($users[0], 6);
+
+    echo sprintf("\r\n" . 'User: %s: ', $user->getName());
+
     foreach ($photos as $photo) {
         $vouteResults = $maxer->setPhotoVoute($photo, 6);
-        echo $vouteResults->getStatusCode();
+        echo sprintf('.');
         sleep(2);
     }
     sleep(10);
